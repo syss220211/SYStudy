ce3496bb3f6d30198716a94f8e3a17e81a19c813
syss220211 <syss220211@gmail.com>
2022-09-12 23:56:29 +0900

    숙제

---
+++ SYStudy/Kakao/ChattingCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ChattingCollectionViewCell.swift
+//  SYStudy
+//
+//  Created by geonhui Yu on 2022/09/12.
+//
+
+import UIKit
+
+class ChattingCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ SYStudy/Kakao/KakaoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  KakaoViewController.swift
+//  SYStudy
+//
+//  Created by geonhui Yu on 2022/09/12.
+//
+
+import UIKit
+
+class KakaoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

6a30544dc6a58b44080314231283ef14e1f32da9
syss220211 <syss220211@gmail.com>
2022-11-08 02:15:56 +0900

    parser example 설명 추가

--- SwiftStudy/Protocol/impl-protocols.playground/Contents.swift
+++ SwiftStudy/Protocol/impl-protocols.playground/Contents.swift
@@ -1,11 +1,13 @@
 import UIKit
 
 protocol Parser {
-    
-    associatedtype Input
+    // associatedtype = 연관 타입
+    associatedtype Input // 모든것이 가능, string, iknt, array,,,
     associatedtype Output
     
     func parse(input: Input) -> Output
+    // func parse(input: String) -> String
+    // 변환의 이유 : HTML에서는 return 배열을 하고 싶어하기 때문에, input, ouput 설정 후 수정
 }
 
 class TextFlipParser: Parser {
@@ -22,6 +24,7 @@ class HTMLParser: Parser {
 
 class JsonParser: Parser {
 
+    // Input, output 생략 가능
     typealias Input = String
     typealias Output = [String:String]
     
@@ -30,8 +33,12 @@ class JsonParser: Parser {
     }
 }
 
-func runParser<P: Parser>(parser: P, input: [P.Input]) where P.Input == JsonParser {
+func runParser<P: Parser>(parser: P, input: [P.Input]) where
+    // p는 타입, 그 타입은 Parser 프로토콜 준수
+    P.Input == JsonParser { // JsonParser 로 Input 제한하기
     input.forEach{
         _ = parser.parse(input: $0)
     }
 }
+
+
